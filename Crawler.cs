66dcde2ecc8f7c7e5640d7e0ba32a444b4c8f37a
dxfferent <o.sokyrka@outlook.com>
2021-09-28 11:29:30 +0100

    Add files via upload
    Maze is the main script here which initialises and draws the map, it has a generate method which is overridden by Crawler or Prims, which have different algorithms and results for generating a maze.

---
+++ Crawler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crawler : Maze
+{
+    public override void Generate()
+    {
+        CrawlHorizontal();
+        CrawlVertical();
+    }
+
+    void CrawlHorizontal()
+    {
+        bool done = false;
+        int x = 1;
+        int z = _depth / 2;
+
+        while (!done)
+        {
+            map[x, z] = 0;
+            if (Random.Range(0, 100) < 50)
+            {
+                x += Random.Range(0, 2);
+            }
+            else
+            {
+                z += Random.Range(-1, 2);
+            }
+            done |= (x < 1 || x >= _width - 1 || z < 1 || z >= _depth - 1);
+        }
+    }
+    void CrawlVertical()
+    {
+        bool done = false;
+        int x = _width / 2;
+        int z = 1;
+
+        while (!done)
+        {
+            map[x, z] = 0;
+            if (Random.Range(0, 100) < 50)
+            {
+                x += Random.Range(-1, 2);
+            }
+            else
+            {
+                z += Random.Range(0, 2);
+            }
+            done |= (x < 1 || x >= _width - 1 || z < 1 || z >= _depth - 1);
+        }
+    }
+}
